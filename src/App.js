b6e0a76c867180a2baef83a0ef982b5c0b31ae35
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-06-26 14:39:03 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import everything here
+
+const App = () => {
+  return (
+    <div>
+      
+      <h1>Buy something!</h1>
+      
+      
+    </div>
+  )
+}
+
+export default App
